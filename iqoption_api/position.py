70b5a371656c636644bcd8c3b67a3393a5ea681f
Dinesh Budhayer <budhayer96d@gmail.com>
2019-05-21 20:03:30 +0545

    almost completed developing the API

--- iqoption_api/position.py
+++ iqoption_api/position.py
@@ -1,10 +1,10 @@
 class Position():
     
-    def __init__(self,data):
+    def __init__(self, data):
         self.__parse_data(data)
     
-    def __parse_data(self,data):
+    def __parse_data(self, data):
         self.__dict__ = data
     
-    def update(self,data):
+    def update(self, data):
         self.__parse_data(data)
