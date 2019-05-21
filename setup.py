712130d22ac48b67d18cdba5b88a4a9ed2fc5a0d
Dinesh Budhayer <budhayer96d@gmail.com>
2019-05-21 22:08:47 +0545

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(
-    name="iqoption-api",
-    version="0.2a",
-    packages=find_packages(),
-    author="Sri Harsha Gangisetty",
-    author_email="sriharshagangisetty@gmail.com",
-    license="Apache License 2.0",
-    url="https://github.com/harwee/IQOption-Api",
-    keywords="IQOption API Python",
-    description="IQOption Api for Python",
+    name='IQOption-Api',
+    version='1.0',
+    url='',
+    license='MIT License',
+    author='ayerdines',
+    author_email='budhayer96d@gmail.com',
+    description='Simple API for IQoption'
 )
